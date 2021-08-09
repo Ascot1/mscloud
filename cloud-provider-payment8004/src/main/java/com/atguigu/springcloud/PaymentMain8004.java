f5f91e2f5e15c83c0f1f6971f2b123429e88d447
Ascot1 <15875006036@163.com>
2021-08-09 20:45:22 +0800

    zookeeper服务提供者

---
+++ cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/PaymentMain8004.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author chendm
+ * @create 2021-08-08 17:48
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8004 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class, args);
+    }
+}
---
+++ cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,25 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author chendm
+ * @create 2021-08-08 17:50
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/payment/zk")
+    public String paymentZk(){
+        return "springcloud with zookeeper:"+serverPort+"\t"+ UUID.randomUUID().toString();
+    }
+}
