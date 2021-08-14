f9d109fcaecb66aee9360e1abc53a1218c810d81
Ascot1 <15875006036@163.com>
2021-08-14 11:38:24 +0800

    完善consul服务提供者

---
+++ cloud-providerconsul-payment8006/src/main/java/com/atguigu/springcloud/Payment8006.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author chendm
+ * @create 2021-08-14 10:58
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Payment8006 {
+    public static void main(String[] args) {
+        SpringApplication.run(Payment8006.class, args);
+    }
+}
---
+++ cloud-providerconsul-payment8006/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,24 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author chendm
+ * @create 2021-08-14 11:07
+ */
+@RestController
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/consul")
+    public String paymentConsul(){
+        return "springcloud with consul: "+serverPort+"\t"+ UUID.randomUUID().toString();
+    }
+}
